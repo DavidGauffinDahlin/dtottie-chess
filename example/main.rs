8fcf6439be8303d399e87a44d2d0fab479a9b63b
Daniel Tottie <danieltottie@hotmail.com>
2021-09-24 12:22:36 +0200

    Create main.rs

---
+++ example/main.rs
@@ -0,0 +1,29 @@
+use dtottie_chess::Game;
+use std::io;
+
+fn main() {
+    let mut game1 = Game::new_game();
+
+    game1.print_board();
+
+    let chesspiece = '\u{2659}';
+    println!("{}", chesspiece);
+
+    loop {
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("failed to read line");
+        let _ = game1.player_move(input);
+        //print!("\x1B[2J\x1B[1;1H");
+        game1.print_board();
+
+        //let id = game1.board[7][0].unique_name;
+        //println!("{}", id);
+    }
+    // let moves = ["E7E6\n", "A2A3\n", "E8E7\n"];
+    // for pmove in moves {
+    //     game1.player_move(pmove.to_owned());
+    //     game1.print_board();
+    // }
+}
